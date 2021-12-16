ea2abf07dd860d19dd62a8aeb9c5a31e5a7221c6
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2021-12-16 17:51:55 -0300

    fadinha

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var starImg,bgImg;
+var star, starBody;
+//criar variável para sprite de fada e imgFada
+var imgFada, fada;
+var vozFada;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+    starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+    //carregar animação de fada
+    imgFada = loadAnimation("images/fairyImage1.png, images/fairyImage2.png");
+    
+}
+
+function setup() {
+    createCanvas(800, 750);
+
+    //escrever código para tocar o som vozFada
+    vozFada = loadSound("sound/JoyMusic.mp3");
+    
+    
+
+    //criar sprite de fada e adicionar animação para fada
+    fada = createSprite(200,30);
+    fada.addImage(imgFada);
+    fada.scale = 0.5;
+    
+    
+    star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+    engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+
+    if(star.y > 470 && starBody.position.y > 470){
+        Matter.Body.setStatic(starBody, true);
+    }
+
+
+
+
+
+   
+}
